8b859c55427bedfdab8ceca3963b3d300d3c143d
HernanTo <hetoro1703@gmail.com>
2023-05-24 15:19:08 -0500

    Docs: Structure

--- login.php
+++ login.php
@@ -1,13 +1,20 @@
 <?php
+session_start();
+include('./conexion.php');
 
 $usuario = $_POST['usuario'];
 $password = $_POST['password'];
+$sql = "SELECT CONCAT(`nombre`, ' ', `apellido`) as nombre, email, documento, usuario_suma, pass FROM usuario WHERE usuario_suma = '$usuario' AND pass = '$password'";
+$resultado = $db->query($sql);
 
-if(isset($_POST['btn-login'])){
-    if($usuario == 'etb' && $password == 'etb'){
-        echo "<script>alert('Usuario correcta');window.location='index.php'</script>";
-    }else{
-            
-        echo "<script>alert('Usuario o contraseña incorrecta');window.location='index.php'</script>";
+if(mysqli_affected_rows($db) > 0){
+    while($row = $resultado->fetch_assoc()){
+        if($row['usuario_suma'] == $usuario && $row['pass'] == $password){
+            $_SESSION['login'] = 1;
+            $_SESSION['nombre'] = $row['nombre'];
+            echo "<script>alert('Usuario correcta');window.location='portal.php'</script>";
+        }else{
+            echo "<script>alert('Usuario o contraseña incorrecta');window.location='index.php'</script>";
+        }
     }
 }
--- portal.php
+++ portal.php
@@ -1,38 +1,56 @@
+<?php 
+    include('seguridad.php')
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/portal.css">
+    <link href="./assets/fontawesome-free-6.4.0-web/css/fontawesome.css" rel="stylesheet">
+    <link href="./assets/fontawesome-free-6.4.0-web/css/brands.css" rel="stylesheet">
+    <link href="./assets/fontawesome-free-6.4.0-web/css/solid.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <div class="breadcump">
-        <a href="#" class="item-bread">Home</a>
-        <a href="#" class="item-bread">Consulta</a>
-        <a href="#" class="item-bread">Vista 360</a>
-        <a href="#" class="item-bread" style="color: rgba(55, 0, 255, 0.623);">Busqueda Cliente</a>
-    </div>
+    <div class="container-genre">
+        <?php 
+            include('./components/infoAudios.php');
+            include('./components/sidebar.php');
+            include('./components/navbar.php');
+        ?>
 
-    <div class="container">
-        <form action="" method="POST">
-            <select name="" id="">
-                <option value="">Tipo de documento</option>
-                <option value="">Cédula de Ciudadania</option>
-                <option value="">Cédula de Extranjeria</option>
-                <option value="">Pasaporte</option>
-            </select>
-            <input type="text" name="" id="" placeholder="Número de Documento">
-            <input type="text" name="" id="" placeholder="Número de Conexión">
-            <input type="text" name="" id="" placeholder="Cuenta Facturación">
-            <input type="text" name="" id="" placeholder="Id Servicio (SALESFORCE)">
-            <input type="text" name="" id="" placeholder="Número de Orden">
-        </form>
-    </div>
-    <div class="container-btn">
-        <button class="btn-search">BUSCAR</button>
-        <button class="btn-empty">LIMPIAR</button>
+        <div class="main-principal-portal">
+            <div class="contedor-portal">
+                <div class="breadcump">
+                    <a href="#" class="item-bread">Home</a>
+                    <a href="#" class="item-bread">Consulta</a>
+                    <a href="#" class="item-bread">Vista 360</a>
+                    <a href="#" class="item-bread" style="color: rgba(55, 0, 255, 0.623);">Busqueda Cliente</a>
+                </div>
+                <form action="">
+                    <div class="con-filtros">
+                            <select name="tipodoc" id="">
+                                <option value="">Tipo de documento</option>
+                                <option value="">Cédula de Ciudadania</option>
+                                <option value="">Cédula de Extranjeria</option>
+                                <option value="">Pasaporte</option>
+                            </select>
+                            <input type="text" name="" id="" placeholder="Número de Documento">
+                            <input type="text" name="" id="" placeholder="Número de Conexión">
+                            <input type="text" name="" id="" placeholder="Cuenta Facturación">
+                            <input type="text" name="" id="" placeholder="Id Servicio (SALESFORCE)">
+                            <input type="text" name="" id="" placeholder="Número de Orden">
+                        </div>
+                        <div class="con-funtion-fil">
+                            <button type="submit" class="btn-clasic"><i class="fa-solid fa-magnifying-glass" style="color: #ffffff;"></i>  BUSCAR</button>
+                            <button class="btn-clasic"><i class="fa-solid fa-broom" style="color: #ffffff;"></i>LIMPIAR</button>
+                        </div>
+                    </div>
+                </form>
+        </div>
     </div>
 </body>
 </html>
