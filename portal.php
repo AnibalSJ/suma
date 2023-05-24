4b3940c2427cddb24248c4ea15090498c5b1fd0b
HernanTo <hetoro1703@gmail.com>
2023-05-24 15:29:12 -0500

    Docs: Structure

--- portal.php
+++ portal.php
@@ -7,8 +7,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/main.css">
     <link rel="stylesheet" href="./css/portal.css">
+    <link rel="stylesheet" href="./css/main.css">
     <link href="./assets/fontawesome-free-6.4.0-web/css/fontawesome.css" rel="stylesheet">
     <link href="./assets/fontawesome-free-6.4.0-web/css/brands.css" rel="stylesheet">
     <link href="./assets/fontawesome-free-6.4.0-web/css/solid.css" rel="stylesheet">
